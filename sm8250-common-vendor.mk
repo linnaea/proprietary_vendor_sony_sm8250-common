e4f467594b0b07189fe96755888ddd3d99c585ec
Michael Bestas <mkbestas@lineageos.org>
2024-09-08 03:07:18 +0300

    sm8250-common: Add xml suffix to prebuilt_etc_xml modules
    Change-Id: I8eec0b86e6ad5e6d460e1b61c3d4d95d5933007c

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -1737,11 +1737,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.callinfo-V1.0-java \
     vendor.qti.ims.factory-V2.0-java \
     vendor.qti.ims.rcsconfig-V1.0-java \
-    android.hardware.gnss@2.1-service-qti \
-    android.hardware.neuralnetworks@1.3-service-qti-hta \
-    android.hardware.neuralnetworks@1.3-service-qti \
-    manifest_android.hardware.drm@1.3-service.widevine \
-    vendor.qti.gnss@4.0-service
+    android.hardware.gnss@2.1-service-qti.xml \
+    android.hardware.neuralnetworks@1.3-service-qti-hta.xml \
+    android.hardware.neuralnetworks@1.3-service-qti.xml \
+    manifest_android.hardware.drm@1.3-service.widevine.xml \
+    vendor.qti.gnss@4.0-service.xml
 
 PRODUCT_PACKAGES += \
     regdb_regdb_symlink
